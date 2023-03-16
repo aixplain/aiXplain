5fbc8db82fa38143783d689629bde05d42853aaf
Thiago Castro Ferreira <thiago.ferreira@aixplain.com>
2023-03-16 12:04:56 +0000

    Updating version file with releasing candidate version

--- aixtend/__version__.py
+++ aixtend/__version__.py
@@ -1,12 +1,12 @@
 __title__ = "aixtend"
 __description__ = "aiXplain SDK adds AI functions to software."
 __url__ = "https://github.com/aixplain/pipelines/tree/main/docs"
-__version__ = "0.0.5"
-__author__ = "Thiago Castro Ferriera, Duraikrishna Selvaraju and Lucas Pavanelli"
+__version__ = "0.0.6rc1"
+__author__ = "aiXplain\'s Engineering and Science teams"
 __author_email__ = "thiago.ferreira@aixplain.com, krishna.durai@aixplain.com, lucas.pavanelli@aixplain.com"
 __license__ = "http://www.apache.org/licenses/LICENSE-2.0"
 __copyright__ = """
-Copyright 2022 The aiXplain SDK authors
+Copyright 2023 The aiXplain SDK authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
