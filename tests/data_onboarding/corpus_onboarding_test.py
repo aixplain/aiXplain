f38b20cf814ecd52f55cf83def51d98f06ecfe50
Thiago Castro Ferreira <thiago.ferreira@aixplain.com>
2023-03-24 21:41:27 +0000

    Unit tests for coorpus onboarding

---
+++ tests/data_onboarding/corpus_onboarding_test.py
@@ -0,0 +1,47 @@
+__author__ = "thiagocastroferreira"
+
+"""
+Copyright 2022 The aiXplain SDK authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import pytest
+from aixtend.enums.language import Language
+from aixtend.enums.license import License
+from aixtend.factories.data_asset_factory import DataAssetFactory
+from uuid import uuid4
+
+
+def test_corpus_onboarding():
+    upload_file = "tests/data_onboarding/input/audio-en_url.csv"
+    schema = [
+        {
+            "name": "audio",
+            "dtype": "audio",
+            "storage_type": "url",
+            "start_column": "audio_start_time",
+            "end_column": "audio_end_time",
+            "languages": [Language.English_UNITED_STATES],
+        },
+        {"name": "text", "dtype": "text", "storage_type": "text", "languages": [Language.English_UNITED_STATES]},
+    ]
+
+    payload = DataAssetFactory.create_corpus(
+        name=str(uuid4()),
+        description="This corpus contain 20 English audios with their corresponding transcriptions.",
+        license=License.MIT,
+        content_path=upload_file,
+        schema=schema,
+    )
+    assert payload["status"] == "onboarding"
