5416947fdf7e7c34118963440c5134195b69f1b4
Lucas Pavanelli <lucas.pavanelli@aixplain.com>
2023-06-13 11:31:16 -0300

    Add BACKEND_URL and MODELS_RUN_URL to .env.example

--- aixplain/utils/config.py
+++ aixplain/utils/config.py
@@ -18,7 +18,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-BACKEND_URL = os.getenv("BACKEND_URL", "https://platform-api.aixplain.com/")
+BACKEND_URL = os.getenv("BACKEND_URL", "https://platform-api.aixplain.com")
 MODELS_RUN_URL = os.getenv("MODELS_RUN_URL", "https://models.aixplain.com/api/v1/execute")
 # GET THE API KEY FROM CMD
 TEAM_API_KEY = os.getenv("TEAM_API_KEY", "")
