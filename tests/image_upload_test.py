d8cb963803e2fa59d768598a1eb4081ed939af53
mikelam-us <michael.lam@aixplain.com>
2023-08-20 17:01:45 -0700

    Create_asset_repo_test
    Signed-off-by: mikelam-us <michael.lam@aixplain.com>

--- tests/image_upload_test.py
+++ tests/image_upload_test.py
@@ -16,12 +16,19 @@ API_FIXED_HEADER = {"x-api-key": f"{config.TEAM_API_KEY}", "Content-Type": "appl
 
 def test_login():
     url =  f"{config.BACKEND_URL}sdk/ecr/login"
-    print(url)
     with requests_mock.Mocker() as mock:
         with open(Path("tests/mock_responses/login_response.json")) as f:
             mock_json = json.load(f)
         mock.post(url, headers=AUTH_FIXED_HEADER, json=mock_json)
         creds = ModelFactory.asset_repo_login(config.TEAM_API_KEY)
-    creds_dict = creds.json()
-    mock_dict = mock_json
-    assert creds_dict == mock_dict
+    assert creds == mock_json
+
+def test_create_asset_repo():
+    url =  f"{config.BACKEND_URL}sdk/models/register"
+    with requests_mock.Mocker() as mock:
+        with open(Path("tests/mock_responses/create_asset_repo_response.json")) as f:
+            mock_json = json.load(f)
+        mock.post(url, headers=API_FIXED_HEADER, json=mock_json)
+        model_id = ModelFactory.create_asset_repo("mock_name", "mock_machines", True, "mock_version", 
+                          "mock_description", "mock_function", False, config.TEAM_API_KEY)
+    assert model_id == mock_json
