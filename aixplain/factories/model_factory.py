b93073b8793a920574374895027208e4643edea8
mikelam-us-aixplain <michael.lam@aixplain.com>
2023-09-12 09:52:48 -0700

    Corrected json -> dict conversion
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- aixplain/factories/model_factory.py
+++ aixplain/factories/model_factory.py
@@ -193,7 +193,7 @@ class ModelFactory:
         else:
             headers = {"x-api-key": f"{cls.api_key}", "Content-Type": "application/json"}
         response = _request_with_retry("get", machines_url, headers=headers)
-        response_dict = dict(response.json())
+        response_dict = json.loads(response.text)
         del response_dict["id"]
         return response_dict
     
