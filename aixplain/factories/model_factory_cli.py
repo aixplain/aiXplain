a994a40bfe9a683d583b07e08bcbafc4f87a722a
mikelam-us-aixplain <michael.lam@aixplain.com>
2023-09-15 11:49:56 -0700

    argument->option
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- aixplain/factories/model_factory_cli.py
+++ aixplain/factories/model_factory_cli.py
@@ -2,7 +2,7 @@ from aixplain.factories.model_factory import ModelFactory
 import click
 
 @click.command("hosts")
-@click.argument('--api-key', default=None, help='Number of greetings.')
+@click.option('--api-key', default=None, help='Number of greetings.')
 def list_host_machines(api_key):
     ret_val = ModelFactory.list_host_machines(api_key)
     click.echo(ret_val)
