0025c2529f3c599027a2b67382e68e563caad881
mikelam-us-aixplain <michael.lam@aixplain.com>
2023-09-15 11:57:34 -0700

    Added list_functions cli
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- aixplain/factories/model_factory_cli.py
+++ aixplain/factories/model_factory_cli.py
@@ -2,12 +2,15 @@ from aixplain.factories.model_factory import ModelFactory
 import click
 
 @click.command("hosts")
-@click.option('--api-key', default=None, help='Number of greetings.')
+@click.option("--api-key", default=None)
 def list_host_machines(api_key):
     ret_val = ModelFactory.list_host_machines(api_key)
     click.echo(ret_val)
     return ret_val
 
 @click.command("functions")
-def list_functions(verbose):
-    pass
+@click.option('--verbose', default=False)
+def list_functions(verbose, api_key):
+    ret_val = ModelFactory.list_functions(verbose, api_key)
+    click.echo(ret_val)
+    return ret_val
