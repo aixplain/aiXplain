f6f2bda0b3d57874569411c1b5cb58ca32c21f1a
mikelam-us-aixplain <michael.lam@aixplain.com>
2023-09-26 16:05:57 -0700

    What if I don't add images?
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- tests/image_upload_e2e_test.py
+++ tests/image_upload_e2e_test.py
@@ -53,10 +53,10 @@ def test_create_and_upload_model():
     registry = login_response["registry"]
 
     # Push an image to ECR
-    low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    low_level_client.pull("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash")
-    low_level_client.tag("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash", f"{registry}/{repo_name}")
-    low_level_client.push(f"{registry}/{repo_name}", auth_config={"username":username, "password":password})
+    # low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    # low_level_client.pull("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash")
+    # low_level_client.tag("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash", f"{registry}/{repo_name}")
+    # low_level_client.push(f"{registry}/{repo_name}", auth_config={"username":username, "password":password})
 
     # Send an email to finalize onboarding process
     ModelFactory.onboard_model(model_id, "latest", "fake_hash")
