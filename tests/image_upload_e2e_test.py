59c8b3151691843f001da2d09f22bcec037d807f
mikelam-us-aixplain <michael.lam@aixplain.com>
2023-09-26 16:04:35 -0700

    Added assert statements
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- tests/image_upload_e2e_test.py
+++ tests/image_upload_e2e_test.py
@@ -9,10 +9,22 @@ import docker
 
 def test_create_and_upload_model():
     # List the host machines
-    ModelFactory.list_host_machines()
+    host_response = ModelFactory.list_host_machines()
+    for hosting_machine_dict in host_response:
+        assert "code" in hosting_machine_dict.keys()
+        assert "type" in hosting_machine_dict.keys()
+        assert "cores" in hosting_machine_dict.keys()
+        assert "memory" in hosting_machine_dict.keys()
+        assert "hourlyCost" in hosting_machine_dict.keys()
 
     # List the functions
-    ModelFactory.list_functions()
+    response = ModelFactory.list_functions()
+    items = response["items"]
+    for item in items:
+        assert "output" not in item.keys()
+        assert "params" not in item.keys()
+        assert "id" not in item.keys()
+        assert "name" in item.keys()
 
     # Register the model, and create an image repository for it.
     with open(Path("tests/test_requests/create_asset_request.json")) as f:
@@ -24,18 +36,24 @@ def test_create_and_upload_model():
     function = register_payload["function"]
     source_language = register_payload["sourceLanguage"]
     register_response = ModelFactory.create_asset_repo(name, host_machine, version, description, function, source_language)
+    assert "id" in register_response.keys()
+    assert "repositoryName" in register_response.keys()
     model_id = register_response["id"]
     repo_name = register_response["repositoryName"]
 
     # Log into the image repository.
     login_response = ModelFactory.asset_repo_login()
+    
+    assert login_response["username"] == "AWS"
+    assert login_response["registry"] == "535945872701.dkr.ecr.us-east-1.amazonaws.com"
+    assert "password" in login_response.keys()
+
     username = login_response["username"]
     password = login_response["password"]
     registry = login_response["registry"]
 
     # Push an image to ECR
     low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    # docker_client = docker.from_env(version='1.41')
     low_level_client.pull("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash")
     low_level_client.tag("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash", f"{registry}/{repo_name}")
     low_level_client.push(f"{registry}/{repo_name}", auth_config={"username":username, "password":password})
