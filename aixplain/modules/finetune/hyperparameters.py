ef438e97eab45865fcb93609a22bb49ba5ba527b
Lucas Pavanelli <lucas.pavanelli@aixplain.com>
2023-12-20 23:28:03 -0300

    Test bound to hyperparameters

--- aixplain/modules/finetune/hyperparameters.py
+++ aixplain/modules/finetune/hyperparameters.py
@@ -13,19 +13,52 @@ class SchedulerType:
     REDUCE_ON_PLATEAU = "reduce_lr_on_plateau"
 
 
+EPOCHS = [1]
+BATCH_SIZE = [1]
+
+
 @dataclass_json
 @dataclass
 class Hyperparameters(object):
-    epochs: int = 4
-    train_batch_size: int = 4
-    eval_batch_size: int = 4
+    epochs: int = 1
+    train_batch_size: int = 1
+    # work with only default values
+    eval_batch_size: int = 1
     learning_rate: float = 2e-5
     generation_max_length: int = 225
-    tokenizer_batch_size: int = 256
     gradient_checkpointing: bool = False
     gradient_accumulation_steps: int = 1
     max_seq_length: int = 4096
     warmup_ratio: float = 0.0
     warmup_steps: int = 0
-    early_stopping_patience: int = 1
     lr_scheduler_type: SchedulerType = SchedulerType.LINEAR
+
+    @property
+    def epochs(self) -> int:
+        return self._epochs
+
+    @epochs.setter
+    def epochs(self, value: int) -> None:
+        if value not in EPOCHS:
+            raise ValueError(f"epochs must be one of the following values: {EPOCHS}")
+        self._epochs = value
+
+    @property
+    def train_batch_size(self) -> int:
+        return self._train_batch_size
+
+    @train_batch_size.setter
+    def train_batch_size(self, value: int) -> None:
+        if value not in BATCH_SIZE:
+            raise ValueError(f"train_batch_size must be one of the following values: {BATCH_SIZE}")
+        self._train_batch_size = value
+
+    @property
+    def eval_batch_size(self) -> int:
+        return self._eval_batch_size
+
+    @eval_batch_size.setter
+    def eval_batch_size(self, value: int) -> None:
+        if value not in BATCH_SIZE:
+            raise ValueError(f"eval_batch_size must be one of the following values: {BATCH_SIZE}")
+        self._eval_batch_size = value
