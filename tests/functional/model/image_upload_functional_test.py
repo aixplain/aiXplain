18eb648213d4a9d00a5b0feee8988848a1ab04e3
mikelam-us-aixplain <michael.lam@aixplain.com>
2024-01-30 07:25:24 -0800

    Adding functional test for all functions
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- tests/functional/model/image_upload_functional_test.py
+++ tests/functional/model/image_upload_functional_test.py
@@ -18,21 +18,23 @@ def test_login():
 
 def test_create_asset_repo():
     with open(Path("tests/test_requests/create_asset_request.json")) as f:
-        mock_register_payload = json.load(f)
-    name = mock_register_payload["name"]
-    description = mock_register_payload["description"]
-    function = mock_register_payload["function"]
-    input_modality = mock_register_payload["input_modality"]
-    output_modality = mock_register_payload["input_modality"]
-    source_language = mock_register_payload["sourceLanguage"]
-    response = ModelFactory.create_asset_repo(name, description, function, input_modality, output_modality, source_language)
-    print(response)
-    response_dict = dict(response)
-    assert "id" in response_dict.keys()
-    assert "repositoryName" in response_dict.keys()
+        mock_register_payload_dict = json.load(f)
+    for key in mock_register_payload_dict.keys():
+        mock_register_payload = mock_register_payload_dict[key]
+        name = mock_register_payload["name"]
+        description = mock_register_payload["description"]
+        function = mock_register_payload["function"]
+        input_modality = mock_register_payload["input_modality"]
+        output_modality = mock_register_payload["input_modality"]
+        source_language = mock_register_payload["sourceLanguage"]
+        response = ModelFactory.create_asset_repo(name, description, function, input_modality, output_modality, source_language)
+        print(response)
+        response_dict = dict(response)
+        assert "id" in response_dict.keys()
+        assert "repositoryName" in response_dict.keys()
 
-    # Test cleanup
-    delete_asset(response["id"], config.TEAM_API_KEY)
+        # Test cleanup
+        delete_asset(response["id"], config.TEAM_API_KEY)
 
 def test_list_host_machines():
     response = ModelFactory.list_host_machines()
