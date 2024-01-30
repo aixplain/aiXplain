cb6a81eaae2b05d31820894a9cc7e5a4c98b8fea
mikelam-us-aixplain <michael.lam@aixplain.com>
2024-01-30 10:18:39 -0800

    Refactored e2e test
    Signed-off-by: mikelam-us-aixplain <michael.lam@aixplain.com>

--- tests/functional/model/image_upload_e2e_test.py
+++ tests/functional/model/image_upload_e2e_test.py
@@ -20,42 +20,44 @@ def test_create_and_upload_model():
 
     # Register the model, and create an image repository for it.
     with open(Path("tests/test_requests/create_asset_request.json")) as f:
-        register_payload = json.load(f)
-    name = register_payload["name"]
-    description = register_payload["description"]
-    function = register_payload["function"]
-    input_modality = register_payload["input_modality"]
-    output_modality = register_payload["input_modality"]
-    source_language = register_payload["sourceLanguage"]
-    register_response = ModelFactory.create_asset_repo(name, description, function, input_modality, output_modality, source_language)
-    assert "id" in register_response.keys()
-    assert "repositoryName" in register_response.keys()
-    model_id = register_response["id"]
-    repo_name = register_response["repositoryName"]
+        register_payload_dict = json.load(f)
+    for key in register_payload_dict.keys():
+        register_payload = register_payload_dict[key]
+        name = register_payload["name"]
+        description = register_payload["description"]
+        function = register_payload["function"]
+        input_modality = register_payload["input_modality"]
+        output_modality = register_payload["input_modality"]
+        source_language = register_payload["sourceLanguage"]
+        register_response = ModelFactory.create_asset_repo(name, description, function, input_modality, output_modality, source_language)
+        assert "id" in register_response.keys()
+        assert "repositoryName" in register_response.keys()
+        model_id = register_response["id"]
+        repo_name = register_response["repositoryName"]
 
-    # Log into the image repository.
-    login_response = ModelFactory.asset_repo_login()
-    
-    assert login_response["username"] == "AWS"
-    assert login_response["registry"] == "535945872701.dkr.ecr.us-east-1.amazonaws.com"
-    assert "password" in login_response.keys()
+        # Log into the image repository.
+        login_response = ModelFactory.asset_repo_login()
+        
+        assert login_response["username"] == "AWS"
+        assert login_response["registry"] == "535945872701.dkr.ecr.us-east-1.amazonaws.com"
+        assert "password" in login_response.keys()
 
-    username = login_response["username"]
-    password = login_response["password"]
-    registry = login_response["registry"]
+        username = login_response["username"]
+        password = login_response["password"]
+        registry = login_response["registry"]
 
-    # Push an image to ECR
-    # os.system("aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 535945872701.dkr.ecr.us-east-1.amazonaws.com")
-    low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    # low_level_client.pull("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash")
-    # low_level_client.tag("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash", f"{registry}/{repo_name}")
-    low_level_client.pull("bash")
-    low_level_client.tag("bash", f"{registry}/{repo_name}")
-    low_level_client.push(f"{registry}/{repo_name}", auth_config={"username":username, "password":password})
+        # Push an image to ECR
+        # os.system("aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 535945872701.dkr.ecr.us-east-1.amazonaws.com")
+        low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        # low_level_client.pull("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash")
+        # low_level_client.tag("535945872701.dkr.ecr.us-east-1.amazonaws.com/bash", f"{registry}/{repo_name}")
+        low_level_client.pull("bash")
+        low_level_client.tag("bash", f"{registry}/{repo_name}")
+        low_level_client.push(f"{registry}/{repo_name}", auth_config={"username":username, "password":password})
 
-    # Send an email to finalize onboarding process
-    ModelFactory.onboard_model(model_id, "latest", "fake_hash")
+        # Send an email to finalize onboarding process
+        ModelFactory.onboard_model(model_id, "latest", "fake_hash")
 
-    # Clean up
-    delete_service_account(config.TEAM_API_KEY)
-    delete_asset(model_id, config.TEAM_API_KEY)
+        # Clean up
+        delete_service_account(config.TEAM_API_KEY)
+        delete_asset(model_id, config.TEAM_API_KEY)
