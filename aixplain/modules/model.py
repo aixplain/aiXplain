a8f9607f9a8ad2fbdb2281e457a4ab9c8f43693b
Thiago Castro Ferreira <thiagocastroferreira@Thiagos-MacBook-Pro-2.local>
2024-03-14 16:53:41 -0300

    Covering some None cases

--- aixplain/modules/model.py
+++ aixplain/modules/model.py
@@ -274,7 +274,11 @@ class Model(Asset):
             resp = r.json()
             finetune_status = AssetStatus(resp["finetuneStatus"])
             model_status = AssetStatus(resp["modelStatus"])
-            logs = sorted(resp["logs"], key=lambda x: (float(x["epoch"]), int(x["step"])))
+            try:
+                logs = sorted(resp["logs"], key=lambda x: (float(x["epoch"]), int(x["step"])))
+            except Exception:
+                # if step is not stored
+                logs = sorted(resp["logs"], key=lambda x: float(x["epoch"]))
 
             if after_epoch is None and after_step is None:
                 logs = logs[-1:]
@@ -282,18 +286,19 @@ class Model(Asset):
                 if after_epoch is not None:
                     logs = [log for log in logs if float(log["epoch"]) >= after_epoch]
                 if after_step is not None:
-                    logs = [log for log in logs if log["step"] >= after_step]
+                    if len(logs) > 0 and "step" in logs[0]:
+                        logs = [log for log in logs if log["step"] >= after_step]
             
             if len(logs) > 0:
                 log = logs[0]
                 status = FinetuneStatus(
                     status=finetune_status,
                     model_status=model_status,
-                    epoch=float(log["epoch"]),
-                    step=int(log["step"]),
-                    learning_rate=float(log["learningRate"]),
-                    training_loss=float(log["trainLoss"]),
-                    validation_loss=float(log["validationLoss"]),
+                    epoch=float(log["epoch"]) if "epoch" in log and log["epoch"] is not None else None,
+                    step=int(log["step"]) if "step" in log and log["step"] is not None else None,
+                    learning_rate=float(log["learningRate"]) if "learningRate" in log and log["learningRate"] is not None else None,
+                    training_loss=float(log["trainLoss"]) if "trainLoss" in log and log["trainLoss"] is not None else None,
+                    validation_loss=float(log["validationLoss"]) if "validationLoss" in log and log["validationLoss"] is not None else None,
                 )
             else:
                 status = FinetuneStatus(
