dd90301b4d02e0b5fd0592aef473ec77e83741ec
Michael Lam <michael.lam@aixplain.com>
2024-05-21 15:56:02 +0000

    Removed user-defined Hugging Face name
    Signed-off-by: Michael Lam <michael.lam@aixplain.com>

--- aixplain/factories/model_factory.py
+++ aixplain/factories/model_factory.py
@@ -406,11 +406,10 @@ class ModelFactory:
         return response
     
     @classmethod
-    def deploy_huggingface_model(cls, name: Text, hf_repo_id: Text, hf_token: Optional[Text] = "", api_key: Optional[Text] = None) -> Dict:
+    def deploy_huggingface_model(cls, hf_repo_id: Text, hf_token: Optional[Text] = "", api_key: Optional[Text] = None) -> Dict:
         """Onboards and deploys a Hugging Face large language model.
 
         Args:
-            name (Text): The user's name for the model.
             hf_repo_id (Text): The Hugging Face repository ID for this model ({author}/{model name}).
             hf_token (Text, optional): Hugging Face access token. Defaults to None.
             api_key (Text, optional): Team API key. Defaults to None.
@@ -425,7 +424,7 @@ class ModelFactory:
             headers = {"Authorization": f"Token {config.TEAM_API_KEY}", "Content-Type": "application/json"}
         body = {
             "model": {
-                "name": name,
+                "name": f"{supplier} {model_name}",
                 "description": "A user-deployed Hugging Face model",
                 "connectionType": ["synchronous"],
                 "function": "text-generation",
