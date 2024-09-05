a02abfa3be5a9437390d0577a794d516967a1b17
xainaz <zaina.abushaban@gmail.com>
2024-09-05 20:00:08 +0300

    Added recent model to LLM test

--- tests/functional/model/run_model_test.py
+++ tests/functional/model/run_model_test.py
@@ -1,13 +1,23 @@
 __author__ = "thiagocastroferreira"
 
-import pytest
 
 from aixplain.enums import Function
 from aixplain.factories import ModelFactory
 from aixplain.modules import LLM
+from datetime import datetime, timedelta, timezone
 
 
-@pytest.mark.parametrize("llm_model", ["Groq Llama 3 70B", "Chat GPT 3.5", "GPT-4o", "GPT 4 (32k)"])
+def pytest_generate_tests(metafunc):
+    if "llm_model" in metafunc.fixturenames:
+        four_weeks_ago = datetime.now(timezone.utc) - timedelta(weeks=4)
+        models = ModelFactory.list(function=Function.TEXT_GENERATION)["results"]
+        recent_models = [model.name for model in models if model.created_at is not None and model.created_at >= four_weeks_ago]
+        if not recent_models:
+            recent_models = ["Groq Llama 3 70B", "Chat GPT 3.5", "GPT-4o", "GPT 4 (32k)"]
+
+        metafunc.parametrize("llm_model", recent_models)
+
+
 def test_llm_run(llm_model):
     """Testing LLMs with history context"""
     model = ModelFactory.list(query=llm_model, function=Function.TEXT_GENERATION)["results"][0]
