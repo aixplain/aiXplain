18dcfecb0dee7b04e7595ab5ed6d89c315c28410
xainaz <zaina.abushaban@gmail.com>
2024-09-09 18:15:20 +0300

    Passing in instantiated model

--- tests/functional/model/run_model_test.py
+++ tests/functional/model/run_model_test.py
@@ -13,19 +13,17 @@ def pytest_generate_tests(metafunc):
         models = ModelFactory.list(function=Function.TEXT_GENERATION)["results"]
 
         predefined_models = ["Groq Llama 3 70B", "Chat GPT 3.5", "GPT-4o", "GPT 4 (32k)"]
-        recent_models = [model.name for model in models if model.created_at is not None and model.created_at >= four_weeks_ago]
-        combined_models = recent_models + predefined_models
-
+        recent_models = [model for model in models if model.created_at and model.created_at >= four_weeks_ago]
+        combined_models = recent_models + [ModelFactory.list(query=model) for model in predefined_models]
         metafunc.parametrize("llm_model", combined_models)
 
 
 def test_llm_run(llm_model):
     """Testing LLMs with history context"""
-    model = ModelFactory.list(query=llm_model, function=Function.TEXT_GENERATION)["results"][0]
 
-    assert isinstance(model, LLM)
+    assert isinstance(llm_model, LLM)
 
-    response = model.run(
+    response = llm_model.run(
         data="What is my name?",
         history=[{"role": "user", "content": "Hello! My name is Thiago."}, {"role": "assistant", "content": "Hello!"}],
     )
