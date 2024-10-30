965304cbc6f52905e4ea9f1e5cd2ff7e324bb954
xainaz <zaina.abushaban@aixplain.com>
2024-10-30 19:48:35 +0300

    added try catch

--- aixplain/modules/agent/__init__.py
+++ aixplain/modules/agent/__init__.py
@@ -252,7 +252,10 @@ class Agent(Model):
             if r.status_code != 200:
                 raise Exception()
         except Exception:
-            response_json = r.json()
-            message = f"Agent Deletion Error (HTTP {r.status_code}): {response_json.get('message')}."
+            try:
+                response_json = r.json()
+                message = f"Agent Deletion Error (HTTP {r.status_code}): {response_json.get('message')}."
+            except ValueError:
+                message = f"Agent Deletion Error (HTTP {r.status_code}): There was an error in deleting the agent."
             logging.error(message)
-            raise Exception(f"{message}")
+            raise Exception(message)
