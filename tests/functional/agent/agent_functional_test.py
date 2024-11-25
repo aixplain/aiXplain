664d081047b3c1395bf41f0bcac2b2915cd1e1e7
Zaina Abu Shaban <zaina.abushaban@aixplain.com>
2024-11-26 00:10:39 +0300

    added tests (#320)

--- tests/functional/agent/agent_functional_test.py
+++ tests/functional/agent/agent_functional_test.py
@@ -163,3 +163,46 @@ def test_delete_agent_in_use(delete_agents_and_team_agents):
     with pytest.raises(Exception) as exc_info:
         agent.delete()
     assert str(exc_info.value) == "Agent Deletion Error (HTTP 403): err.agent_is_in_use."
+
+
+def test_update_tools_of_agent(run_input_map, delete_agents_and_team_agents):
+    assert delete_agents_and_team_agents
+
+    agent = AgentFactory.create(
+        name=run_input_map["agent_name"], description=run_input_map["agent_name"], llm_id=run_input_map["llm_id"]
+    )
+    assert agent is not None
+    assert agent.status == AssetStatus.DRAFT
+    assert len(agent.tools) == 0 
+
+    tools = []
+    if "model_tools" in run_input_map:
+        for tool in run_input_map["model_tools"]:
+            tool_ = copy.copy(tool)
+            for supplier in Supplier:
+                if tool["supplier"] is not None and tool["supplier"].lower() in [
+                    supplier.value["code"].lower(),
+                    supplier.value["name"].lower(),
+                ]:
+                    tool_["supplier"] = supplier
+                    break
+            tools.append(AgentFactory.create_model_tool(**tool_))
+
+    if "pipeline_tools" in run_input_map:
+        for tool in run_input_map["pipeline_tools"]:
+            tools.append(AgentFactory.create_pipeline_tool(pipeline=tool["pipeline_id"], description=tool["description"]))
+
+    agent.tools = tools
+    agent.update()
+
+    agent = AgentFactory.get(agent.id)
+    assert len(agent.tools) == len(tools)  
+
+    removed_tool = agent.tools.pop()
+    agent.update()
+
+    agent = AgentFactory.get(agent.id)
+    assert len(agent.tools) == len(tools) - 1 
+    assert removed_tool not in agent.tools 
+
+    agent.delete()
