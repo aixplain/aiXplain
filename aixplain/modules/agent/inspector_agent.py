f2506642a0592d4c0cff13d188d855b051069688
Yunsu Kim <yunsu.kim@aixplain.com>
2025-04-23 13:05:13 +0200

    Add InspectorAgent

---
+++ aixplain/modules/agent/inspector_agent.py
@@ -0,0 +1,55 @@
+"""Pre-defined agent for inspecting the data flow within a team agent.
+
+Example usage:
+
+inspector = InspectorAgent(
+    model_id="my_model",
+    model_config={"prompt": "Check if the data is safe to use."},
+    policy=InspectorPolicy.ADAPTIVE
+)
+
+team = TeamAgent(
+    name="team"
+    agents=agents,
+    description="team description",
+    llm_id="xyz",
+    use_mentalist=True,
+    inspectors=[inspector],
+)
+"""
+
+from enum import Enum
+from typing import Dict, Optional
+
+from pydantic import field_validator
+
+from aixplain.modules.agent.model_with_params import ModelWithParams
+
+
+class InspectorPolicy(str, Enum):
+    """Which action to take if the inspector gives negative feedback."""
+
+    WARN = "warn"  # log only, continue execution
+    ABORT = "abort"  # stop execution
+    ADAPTIVE = "adaptive"  # adjust execution according to feedback
+
+
+class Inspector(ModelWithParams):
+    """Pre-defined agent for inspecting the data flow within a team agent.
+
+    The model should be onboarded before using it as an inspector.
+
+    Attributes:
+        model_id: The ID of the model to wrap.
+        model_config: The configuration for the model.
+        policy: The policy for the inspector. Default is ADAPTIVE.
+    """
+
+    model_config: Optional[Dict] = None
+    policy: InspectorPolicy = InspectorPolicy.ADAPTIVE
+
+    @field_validator("policy")
+    def validate_policy(cls, v: InspectorPolicy) -> InspectorPolicy:
+        if v not in InspectorPolicy:
+            raise ValueError(f"Invalid policy: {v}")
+        return v
