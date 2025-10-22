ed143729d0233b8e8de6e899a5458504cabc6b62
Zaina Abu Shaban <zaina.abushaban@aixplain.com>
2025-10-22 22:17:05 +0300

    PROD-1954-added inspector in build agent (#704)

--- aixplain/modules/team_agent/inspector.py
+++ aixplain/modules/team_agent/inspector.py
@@ -58,7 +58,7 @@ class InspectorOutput(BaseModel):
 
 class InspectorAuto(str, Enum):
     """A list of keywords for inspectors configured automatically in the backend."""
-
+    ALIGNMENT = "alignment" 
     CORRECTNESS = "correctness"
 
     def get_name(self) -> Text:
