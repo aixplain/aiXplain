f6d86d36c67db5f27e7e1bedb17381eb1341e4ed
zaina.abushaban <zaina.abushaban@aixplain.com>
2026-01-22 11:38:10 +0100

    changed to subagents

--- tests/functional/v2/inspector_functional_test.py
+++ tests/functional/v2/inspector_functional_test.py
@@ -65,7 +65,7 @@ def _make_team_agent(client, timestamp: str, agents, inspectors):
     team_agent = client.Agent(
         name="InspectortestTeam_" + timestamp,
         description="Team agent with Inspector v2 functional test",
-        agents=agents,
+        subagents=agents,
         inspectors=inspectors,
     )
     team_agent.save()
